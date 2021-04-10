f7f5fd1c04076012a42cb2b9b27fe5c6e916afd7
Khaled S Momani <khaledmomani4@gmail.com>
2021-04-10 16:01:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,15 +4,15 @@ end
 
 
 def sort_array_desc(array)
-array.sort do |a, b|
-  b <=> a
-end
+  array.sort do |a, b|
+    b <=> a
+  end
 end
 
 def sort_array_char_count(array)
-  array.sort do |a, b|
-  a.length <=> b.length
-end
+    array.sort do |a, b|
+    a.length <=> b.length
+  end
 end
 
 def swap_elements(array)
